e6591f9d0b25a3db0cadfdcc534896f0c31db696
Honcharenko Danylo Dmytrovych <danilo.gonch@gmail.com>
2023-11-18 15:30:41 +0200

    Made a loader for the About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,65 +8,84 @@ import {getCV} from "../utils/rest/swr.js";
 
 const About = () => {
 
-    const {cv} = getCV();
+    const {cv, isLoading} = getCV()
 
     return (
         <Main>
             <TitleBar title="Who i am? (CV)" />
-            <Box isPadding={true} className="pt-3 pb-3 w-fit ml-auto mr-auto">
-                <div className="flex gap-3">
-                    <div>
-                        <img src={PortfolioPhoto} alt="Portfolio Photo" width="200px"/>
+            {isLoading ?
+                <Box isPadding={true} className="pt-3 pb-3 w-fit ml-auto mr-auto">
+                    <div className="flex gap-3">
+                        <div className="bg-slate-200 w-48 h-64"></div>
+                        <div>
+                            <div className="bg-slate-200 w-80 h-9"></div>
+                            <div className="bg-slate-200 w-80 h-6 mt-1"></div>
+                            <div className="bg-slate-200 w-80 h-6 mt-1"></div>
+                            <div className="bg-slate-200 w-80 h-6 mt-1"></div>
+                            <div className="bg-slate-200 w-80 h-6 mt-1"></div>
+                        </div>
                     </div>
                     <div>
+                        <div className="bg-slate-200 w-80 h-6 mt-1"></div>
+                        <div className="bg-slate-200 w-11/12 h-48 mt-1"></div>
+                    </div>
+                </Box>
+                :
+                <Box isPadding={true} className="pt-3 pb-3 w-fit ml-auto mr-auto">
+                    <div className="flex gap-3">
                         <div>
-                            <Title>{cv?.Title}</Title>
+                            <img src={PortfolioPhoto} alt="Portfolio Photo" width="200px"/>
                         </div>
                         <div>
-                            <nav>
-                                <ul>
-                                    <li>{cv?.NameAndSurname}</li>
-                                    <li><b>Telephone:</b> {cv?.Telephone}</li>
-                                    <li><b>Email:</b> {cv?.Email}</li>
-                                    <li><b>City:</b> {cv?.City}</li>
-                                    <li><b>Date of birth:</b> {cv?.DateOfBirth}</li>
-                                </ul>
-                            </nav>
                             <div>
-                                <a href={import.meta.env.VITE_BASE_SERVER_URL + cv?.cvPdf.url} target="_blank"><button className="bg-[#F1F1F1] p-2 hover:bg-[#E3E3E3] mt-3">Download CV .pdf</button></a>
+                                <Title>{cv?.Title}</Title>
+                            </div>
+                            <div>
+                                <nav>
+                                    <ul>
+                                        <li>{cv?.NameAndSurname}</li>
+                                        <li><b>Telephone:</b> {cv?.Telephone}</li>
+                                        <li><b>Email:</b> {cv?.Email}</li>
+                                        <li><b>City:</b> {cv?.City}</li>
+                                        <li><b>Date of birth:</b> {cv?.DateOfBirth}</li>
+                                    </ul>
+                                </nav>
+                                <div>
+                                    <a href={import.meta.env.VITE_BASE_SERVER_URL + cv?.cvPdf.url} target="_blank"><button className="bg-[#F1F1F1] p-2 hover:bg-[#E3E3E3] mt-3">Download CV .pdf</button></a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div>
                     <div>
-                        <h1 className="font-bold text-xl">Resume summary</h1>
+                        <div>
+                            <h1 className="font-bold text-xl">Resume summary</h1>
+                        </div>
+                        <div className="w-auto lt:w-[530px]">
+                            <p>{cv?.ResumeSummary}</p>
+                        </div>
+                        <div className="mb-2">
+                            <p><b>Employment:</b> {cv?.Employment}</p>
+                        </div>
+                        <hr/>
+                        <div className="w-auto lt:w-[530px] mt-2 mb-2">
+                            <pre>{cv?.Skills}</pre>
+                        </div>
+                        <hr/>
+                        <div className="mt-2">
+                            <p><b>Education:</b></p>
+                        </div>
+                        <div>
+                            <p>{cv?.Education}</p>
+                        </div>
+                        <div>
+                            <p><b>Speciality:</b> {cv?.Speciality}</p>
+                        </div>
+                        <div>
+                            <p><b>Foreign languages:</b> {cv?.ForeignLanguages}</p>
+                        </div>
                     </div>
-                    <div className="w-auto lt:w-[530px]">
-                        <p>{cv?.ResumeSummary}</p>
-                    </div>
-                    <div className="mb-2">
-                        <p><b>Employment:</b> {cv?.Employment}</p>
-                    </div>
-                    <hr/>
-                    <div className="w-auto lt:w-[530px] mt-2 mb-2">
-                        <pre>{cv?.Skills}</pre>
-                    </div>
-                    <hr/>
-                    <div className="mt-2">
-                        <p><b>Education:</b></p>
-                    </div>
-                    <div>
-                        <p>Rocket-and-Space Engineering College of Oles Honchar Dnipro National University</p>
-                    </div>
-                    <div>
-                        <p><b>Speciality:</b> Computer Engineering</p>
-                    </div>
-                    <div>
-                        <p><b>Foreign languages:</b> English (A1)</p>
-                    </div>
-                </div>
-            </Box>
+                </Box>
+            }
         </Main>
     );
 };
